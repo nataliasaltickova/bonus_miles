85db360595b4ca27d15a2a1ff184546426184af6
nataliasaltickova <nataliasaltickova@yandex.ru>
2022-03-17 18:39:32 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 100;
+        int costMile = 20;
+
+        System.out.println("Вы получили " +  (ticket / costMile) + " бонусных миль");
+    }
+}
